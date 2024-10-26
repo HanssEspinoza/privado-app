3d99cacfc8a90df3a0afc187a9ff12d175c52abe
Hanss Espinoza <hanssabinadie@gmail.com>
2024-10-26 04:48:54 -0600

    delete dist

--- backend/dist/config/envs.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.envs = void 0;
-require("dotenv/config");
-const env_var_1 = require("env-var");
-exports.envs = {
-    PORT: (0, env_var_1.get)("PORT").required().asPortNumber(),
-    JWT_SECRET: (0, env_var_1.get)("JWT_SECRET").required().asString(),
-};
